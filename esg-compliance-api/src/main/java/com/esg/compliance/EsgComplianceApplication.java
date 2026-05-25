692cffe5c3bb5d9ca7bbf5cbce90d6a4d1b6544e
Gustavo Gomes <gustavo.gomesmmixx@outlook.com.br>
2026-05-25 13:16:16 -0300

    fix: configuração do banco de dados via código

--- esg-compliance-api/src/main/java/com/esg/compliance/EsgComplianceApplication.java
+++ esg-compliance-api/src/main/java/com/esg/compliance/EsgComplianceApplication.java
@@ -2,11 +2,29 @@ package com.esg.compliance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Properties;
 
 @SpringBootApplication
 public class EsgComplianceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EsgComplianceApplication.class, args);
+        SpringApplication app = new SpringApplication(EsgComplianceApplication.class);
+
+        Properties props = new Properties();
+        props.put("spring.datasource.url", "jdbc:oracle:thin:@oracle.fiap.com.br:1521/ORCL");
+        props.put("spring.datasource.username", "rm566105");
+        props.put("spring.datasource.password", "fiap25");
+        props.put("spring.datasource.driver-class-name", "oracle.jdbc.OracleDriver");
+        props.put("spring.jpa.database-platform", "org.hibernate.dialect.OracleDialect");
+        props.put("spring.jpa.hibernate.ddl-auto", "none");
+        props.put("spring.jpa.show-sql", "true");
+        props.put("spring.flyway.enabled", "true");
+        props.put("spring.flyway.locations", "classpath:db/migration");
+        props.put("spring.flyway.baseline-on-migrate", "true");
+        props.put("spring.datasource.hikari.maximum-pool-size", "2");
+        props.put("spring.datasource.hikari.minimum-idle", "1");
+
+        app.setDefaultProperties(props);
+        app.run(args);
     }
 }
